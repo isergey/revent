7e44499cc3a1f5e7867d542d2fc1f1f7f231ee71
Sergey Dostovalov <dostovalov@gmail.com>
2015-06-18 15:33:31 +0300

    webpack

---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+var path = require('path')
+var webpack = require('webpack')
+
+module.exports = {
+  entry: path.join(__dirname, 'src', 'index.js'),
+  output: {
+    filename: path.join(__dirname, 'dist', 'revent.js')
+  },
+  module: {
+    loaders: [
+      { test: /\.js$/,
+        loader: 'babel-loader'
+      }
+    ]
+  }
+}
